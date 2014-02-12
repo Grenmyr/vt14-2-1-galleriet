0e1339bb3cffde5b121ac22bf2f3ea4d7c298b9d
Grenmyr <dg222cs@student.lnu.se>
2014-02-12 11:52:44 +0100

    commit

--- galleriet/galleriet/Model/Gallery.cs
+++ galleriet/galleriet/Model/Gallery.cs
@@ -10,22 +10,49 @@ namespace galleriet.Model
 {
     public class Gallery
     {
-        Regex ApprovedExtensions;
-        Regex SantizePath;
-        string PhysicalUploadedImagesPath;
+        private static readonly Regex ApprovedExtensions;
+        private static readonly Regex SantizePath;
+        private static string PhysicalUploadedImagesPath;
+        private static string PhysicalUploadedThumbNailPath;
 
-        // Konstruktor
-        private static Gallery()
+        // Constructor
+        static Gallery()
         {
+            //Setting Regex pattern to field.
+            string pattern = @"^.*\.(gif|jpg|png)$";
+            ApprovedExtensions = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            //Setting physical direction to my files
+            PhysicalUploadedImagesPath = Path.Combine(AppDomain.CurrentDomain.GetData("APPBASE").ToString(), @"Files\Images");
+            PhysicalUploadedThumbNailPath = Path.Combine(AppDomain.CurrentDomain.GetData("APPBASE").ToString(), @"Files\Thumbnails");
+
+            // "GetInvalidFileNameChars()" is a built in collection of illegal chars, which i after saving into variable "invalidchars".
+            // Using my expression "invalidchars "to set my field "sanitizePath" IF the regex escape "invalidchars"
+            var invalidChars = new string(Path.GetInvalidFileNameChars());
+            SantizePath = new Regex(string.Format("[{0}]", Regex.Escape(invalidChars)));
         }
 
         public IEnumerable<string> GetImageNames()
         {
+            // Getting files from the path saving them into an array.
+            var images = new DirectoryInfo(PhysicalUploadedImagesPath).GetFiles();
+            List<string> imagesList = new List<string>(50);
 
-            throw new NotImplementedException("Tom metod");
+            for (int i = 0; i < images.Length; i++)
+            {
+                imagesList.Add(images[i].ToString());
+            }
+            
+            // Using "Select" loop my list to match against Regexobject approved extensions.
+            imagesList.Select(imageName => ApprovedExtensions.IsMatch(imageName));
+            imagesList.TrimExcess();
+            imagesList.Sort();
+
+            return imagesList.AsReadOnly(); 
+
         }
 
-        public bool ImageExist(string name)
+        public  static bool ImageExist(string name)
         {
             throw new NotImplementedException("tom imageexistmetod");
         }
